a7c51e948525b3988e085c3fdcd50f7302af6569
Manish Kumar <mkc1020304050@gmail.com>
2024-12-10 04:41:50 +0530

    login page added

---
+++ src/store/userStore.js
@@ -0,0 +1,15 @@
+import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
+
+export const userStore = create(
+  persist(
+    (set, get) => ({
+      user: {},
+      setUser: (newUser) => set({ user: newUser}),
+    }),
+    {
+      name: 'user-store', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    },
+  ),
+)
