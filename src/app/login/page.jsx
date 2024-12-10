7ad4e88f31757d9e39db6afaeaad14aeb0477b5f
Manish Kumar <mkc1020304050@gmail.com>
2024-12-10 16:22:11 +0530

    login page changed

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,9 @@
-"use client"
-import axios from "axios"
-import { useToast } from "@/components/hooks/use-toast"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+"use client";
+import axios from "axios";
+import { useToast } from "@/components/hooks/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import {
   Form,
   FormControl,
@@ -12,147 +12,154 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
+} from "@/components/ui/form";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useState } from "react"
-import { userStore } from "@/store/userStore"
-
-
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { userStore } from "@/store/userStore";
 
 const FormSchema = z.object({
-    email: z.string()
-    // (2, {
-    //   message: "Invalid Email.",
-    // })
-    ,
-    password: z.string().min(2, {
-      message: "Password must be at least 2 characters.",
-    }),
-  })
-
-
-
+  email: z.string().min(2, {
+    message: "Invalid Email.",
+  }),
+  password: z.string().min(2, {
+    message: "Password must be at least 2 characters.",
+  }),
+});
 
 function LoginForm() {
-    const user = userStore(((state) => state.user));
-    const setUser = userStore(((state) => state.setUser));
-    const {toast} = useToast();
-    const form = useForm({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-          email: "",
-          password:""
-        },
-      })
+  const user = userStore((state) => state.user);
+  const setUser = userStore((state) => state.setUser);
+  const { toast } = useToast();
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-     
-      async function onSubmit(data) {
-        // toast({
-        //   title: "You submitted the following values:",
-        //   description: (
-        //     <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-        //       <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        //     </pre>
-        //   ),
-        // })
-        console.log(data);
-        const response = await axios.post("http://localhost:8085/v1/auth/login",data)
-        setUser(response.data);
-        console.log(response);
+  async function onSubmit(data) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+    console.log(data);
+    const response = await axios.post(
+      "http://localhost:8085/v1/auth/login",
+      data
+    );
+    setUser(response.data);
+    console.log(response);
+  }
 
-      }
-     
   return (
-    <Card className="mx-auto my-20 max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
-                    <div className="grid gap-2">
-                        <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        id="email"
-                                        type="text"
-                                        placeholder="Enter Your Email"
-                                        {...field}
-                                    />
-                                </FormControl>
-                            
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                        />
-                    </div>
-                    <div className="grid gap-2">
-                        <FormField
-                        control={form.control}
-                        name="password"
-                        render={({ field }) => (
-                            <FormItem>
-                                <div className="flex items-center">
-                                    <FormLabel>Password</FormLabel>
-                                    <Link href="#" className="ml-auto inline-block text-sm underline">
-                                        Forgot your password?
-                                    </Link>
-                                </div>
-                                <FormControl>
-                                    <Input
-                                        id="password"
-                                        type="password"
-                                        placeholder="Enter Your Password"
-                                        {...field}
-                                    />
-                                </FormControl>
-                            
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                        />
-                    </div>
-                    <Button className="w-full" type="submit" >
-                        Login
-                    </Button>
-                    <Button variant="outline" className="w-full">
-                        Login with Google
-                    </Button>
+    <div className="flex flex-col md:flex-row h-screen">
+      {/* Left Section */}
+      <div className="bg-black w-full md:w-1/2  flex items-center justify-center">
+        <div className="text-white font-extrabold font-serif text-3xl md:text-5xl tracking-wide drop-shadow-lg">
+          WorkHub
+        </div>
+
+      </div>
+
+      {/* Right Section */}
+      <div className=" w-full md:w-1/2 flex items-center justify-center">
+        <Card className="mx-auto my-20 max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl text-center">Login</CardTitle>
+            <CardDescription>
+              Enter your credentials below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-4">
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="w-full space-y-6"
+                >
+                  <div className="grid gap-2">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              id="email"
+                              type="email"
+                              placeholder="Enter Your Email"
+                              {...field}
+                            />
+                          </FormControl>
+
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <div className="flex items-center">
+                            <FormLabel>Password</FormLabel>
+                            <Link
+                              href="#"
+                              className="ml-auto inline-block text-sm underline"
+                            >
+                              Forgot your password?
+                            </Link>
+                          </div>
+                          <FormControl>
+                            <Input
+                              id="password"
+                              type="password"
+                              placeholder="Enter Your Password"
+                              {...field}
+                            />
+                          </FormControl>
+
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <Button className="w-full" type="submit">
+                    Login
+                  </Button>
+                  <Button variant="outline" className="w-full">
+                    Login with Google
+                  </Button>
                 </form>
-            </Form>
-
-
-
-
-
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
-            Register
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
-  )
+              </Form>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link href="#" className="underline">
+                Register
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
 export default LoginForm;
